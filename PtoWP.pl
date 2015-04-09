8079ead8938606ad6e1674527bb3ed43f1bb864f
yshan <yshan@localhost.localdomain>
2015-04-08 23:28:48 -0400

    git tracks changes

--- PtoWP.pl
+++ PtoWP.pl
@@ -17,3 +17,7 @@ $_ =~ s/=//;
 
 #Git is a distributed version control system.
 #Git is free software distributed under the GPL.
+#Git is a distributed version control system.
+#Git is free software distributed under the GPL.
+#Git has a mutable index called stage.
+#Git tracks changes.
